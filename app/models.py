fd0c1e2252840c69d42885f317aa02f8f56088e4
Shayne Sivley <shayne@fennon.com>
2026-04-07 10:17:55 -0600

    File One Time Access

--- app/models.py
+++ app/models.py
@@ -167,6 +167,7 @@ class AppSettings(models.Model):
     otf_brand_name = models.CharField(max_length=255, blank=True, default='KineticLull Secure File Access', verbose_name='File Share Brand Name')
     otf_brand_bg_color = models.CharField(max_length=7, blank=True, default='#1a1d21', verbose_name='Brand Background Color')
     otf_brand_text_color = models.CharField(max_length=7, blank=True, default='#ffffff', verbose_name='Brand Text Color')
+    otf_brand_card_color = models.CharField(max_length=7, blank=True, default='#ffffff', verbose_name='Card Background Color')
     otf_brand_image = models.FileField(upload_to='branding/', blank=True, verbose_name='Brand Logo')
 
     # Deployment
